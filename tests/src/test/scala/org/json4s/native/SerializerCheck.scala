9c1354a8abe402c4c4a44cd61789d6eddc8019c6
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-12-29 20:18:40 +0100

    enable scalacheck and reverse formats again

--- tests/src/test/scala/org/json4s/native/SerializerCheck.scala
+++ tests/src/test/scala/org/json4s/native/SerializerCheck.scala
@@ -1,50 +1,50 @@
-//package org.json4s
-//
-//import org.json4s.native.{ Serialization => NSerialization }
-//import NSerialization._
-//
-//import org.scalacheck._
-//import Prop._
-//
-//object SerializerCheck extends Properties("serializer") {
-//
-//  implicit val formats = NSerialization formats NoTypeHints
-//
-//  def alphaNumStr: Gen[String] = for {
-//    cs <- Gen listOf Gen.alphaNumChar
-//  } yield cs.mkString
-//
-//  def p1 = (
-//    "In *double mode* any json document can be serialized into a string and " +
-//    "deserialized into an equivalent json document."
-//  )
-//  property(p1) = {
-//    implicit val arbJson = ArbitraryJson4s.arbJValueDouble
-//    forAll { json: JValue => serDeserAreInverses(json, false) }
-//  }
-//
-//  def p2 = (
-//    "In *decimal mode* any json document can be serialized into a string and " +
-//    "deserialized into an equivalent json document."
-//  )
-//  property(p2) = {
-//    implicit val arbJson = ArbitraryJson4s.arbJValueDecimal
-//    forAll { json: JValue => serDeserAreInverses(json, true) }
-//  }
-//
-//  def serDeserAreInverses(json: JValue, decimalMode: Boolean): Boolean = {
-//    type SerType = List[JValue]
-//    val lst1: SerType = List(json)
-//    val lst2: SerType = read[SerType](
-//      write[SerType](List(json)),
-//      decimalMode
-//    )
-//    val res = lst1 == lst2
-//    if(!res) {
-//      println("input: " + lst1)
-//      println("deser: " + lst2)
-//      println("*" * 80)
-//    }
-//    res
-//  }
-//}
+package org.json4s
+
+import org.json4s.native.{ Serialization => NSerialization }
+import NSerialization._
+
+import org.scalacheck._
+import Prop._
+
+object SerializerCheck extends Properties("serializer") {
+
+  implicit val formats = NSerialization formats NoTypeHints
+
+  def alphaNumStr: Gen[String] = for {
+    cs <- Gen listOf Gen.alphaNumChar
+  } yield cs.mkString
+
+  def p1 = (
+    "In *double mode* any json document can be serialized into a string and " +
+    "deserialized into an equivalent json document."
+  )
+  property(p1) = {
+    implicit val arbJson = ArbitraryJson4s.arbJValueDouble
+    forAll { json: JValue => serDeserAreInverses(json, false) }
+  }
+
+  def p2 = (
+    "In *decimal mode* any json document can be serialized into a string and " +
+    "deserialized into an equivalent json document."
+  )
+  property(p2) = {
+    implicit val arbJson = ArbitraryJson4s.arbJValueDecimal
+    forAll { json: JValue => serDeserAreInverses(json, true) }
+  }
+
+  def serDeserAreInverses(json: JValue, decimalMode: Boolean): Boolean = {
+    type SerType = List[JValue]
+    val lst1: SerType = List(json)
+    val lst2: SerType = read[SerType](
+      write[SerType](List(json)),
+      decimalMode
+    )
+    val res = lst1 == lst2
+    if(!res) {
+      println("input: " + lst1)
+      println("deser: " + lst2)
+      println("*" * 80)
+    }
+    res
+  }
+}
--- tests/src/test/scala/org/json4s/native/arbitrary.scala
+++ tests/src/test/scala/org/json4s/native/arbitrary.scala
@@ -1,113 +1,113 @@
-//package org.json4s
-//
-//import org.scalacheck._
-//import Gen._
-//import Arbitrary.arbitrary
-//
-//import org.json4s._
-//
-//object ArbitraryJson4s {
-//  //TODO allow for larger trees
-//  //TODO use ScalaCheck's size param
-//  val MaxSize = 1
-//
-//  val genJValueDouble: Gen[JValue] = new Nodes(false).genJValue
-//  val arbJValueDouble: Arbitrary[JValue] = Arbitrary(genJValueDouble)
-//
-//  val genJValueDecimal: Gen[JValue] = new Nodes(true).genJValue
-//  val arbJValueDecimal: Arbitrary[JValue] = Arbitrary(genJValueDecimal)
-//
-//  // def genJDecimal: Gen[JDecimal] = { //TODO use better BigDecimal generation
-//  //   val gen: Gen[JDecimal] = for {
-//  //     d <- arbitrary[BigDecimal]
-//  //   } yield JDecimal(d)
-//  //   gen suchThat {
-//  //     case JDecimal(bd) => try {
-//  //       bd.underlying.toBigIntegerExact
-//  //       false
-//  //     }
-//  //     catch {
-//  //       case _ => true
-//  //     }
-//  //   } suchThat {
-//  //     case JDecimal(bd) => try {
-//  //       BigDecimal(bd.toString)
-//  //       true
-//  //     }
-//  //     catch {
-//  //       case _ => false
-//  //     }
-//  //   }
-//  // }
-//  def genJDecimal: Gen[JDecimal] = for {
-//    d <- arbitrary[Double]
-//  } yield JDecimal(BigDecimal(d))
-//  def arbJDecimal: Arbitrary[JDecimal] = Arbitrary(genJDecimal)
-//
-//  def genJDouble: Gen[JDouble] = for {
-//    d <- arbitrary[Double]
-//  } yield JDouble(d)
-//  def arbJDouble: Arbitrary[JDouble] = Arbitrary(genJDouble)
-//
-//  def genJString: Gen[JString] = for {
-//    s <- arbitrary[String]
-//  } yield JString(s)
-//  def arbJString: Arbitrary[JString] = Arbitrary(genJString)
-//
-//  def genJBool: Gen[JBool] = for {
-//    b <- arbitrary[Boolean]
-//  } yield JBool(b)
-//  def arbJBool: Arbitrary[JBool] = Arbitrary(genJBool)
-//
-//  def genJInt: Gen[JInt] = for {
-//    l <- arbitrary[Long]
-//  } yield JInt(l)
-//  def arbJInt: Arbitrary[JInt] = Arbitrary(genJInt)
-//
-//  def genJNull: Gen[JNull.type] = Gen value JNull
-//  def arbJNull: Arbitrary[JNull.type] = Arbitrary(genJNull)
-//
-//  def genJNothing: Gen[JNothing.type] = Gen value JNothing
-//  def arbJNothing: Arbitrary[JNothing.type] = Arbitrary(genJNothing)
-//
-//  class Nodes(decimalMode: Boolean) {
-//
-//    implicit val arb = Arbitrary(genJValue)
-//    private[this] implicit val arbObj = arbGenJObject
-//
-//    def genJValue: Gen[JValue] = Gen frequency (
-//      (75, genJValueSansFloat),
-//      (25, if (decimalMode) genJDecimal else genJDouble)
-//    )
-//
-//    def genJValueSansFloat: Gen[JValue] = Gen frequency (
-//      (15, genJNull),
-//      (15, genJString),
-//      (15, genJInt),
-//      (15, genJBool),
-//      (30, genJObject),
-//      (30, genJArray)
-//    )
-//
-//    def genJField: Gen[JField] = for {
-//      s <- arbitrary[String]
-//      v <- arbitrary[JValue]
-//    } yield (s, v)
-//    def arbGenJField: Arbitrary[JField] = Arbitrary(genJField)
-//
-//    def genJObject: Gen[JObject] = for {
-//      lst: List[JField] <- containerOfN[List, JField](MaxSize, genJField)
-//    } yield JObject(lst)
-//    def arbGenJObject: Arbitrary[JObject] = Arbitrary(genJObject)
-//
-//    def genJArray: Gen[JArray] = for {
-//      obj: JObject <- arbitrary[JObject]
-//    } yield JArray {
-//      val jvals: List[JValue] = obj.fold(List.empty[JValue]) {
-//        (acc, el) => el :: acc
-//      }
-//      jvals
-//    }
-//    def arbGenJArray: Arbitrary[JArray] = Arbitrary(genJArray)
-//  }
-//}
+package org.json4s
+
+import org.scalacheck._
+import Gen._
+import Arbitrary.arbitrary
+
+import org.json4s._
+
+object ArbitraryJson4s {
+  //TODO allow for larger trees
+  //TODO use ScalaCheck's size param
+  val MaxSize = 1
+
+  val genJValueDouble: Gen[JValue] = new Nodes(false).genJValue
+  val arbJValueDouble: Arbitrary[JValue] = Arbitrary(genJValueDouble)
+
+  val genJValueDecimal: Gen[JValue] = new Nodes(true).genJValue
+  val arbJValueDecimal: Arbitrary[JValue] = Arbitrary(genJValueDecimal)
+
+  // def genJDecimal: Gen[JDecimal] = { //TODO use better BigDecimal generation
+  //   val gen: Gen[JDecimal] = for {
+  //     d <- arbitrary[BigDecimal]
+  //   } yield JDecimal(d)
+  //   gen suchThat {
+  //     case JDecimal(bd) => try {
+  //       bd.underlying.toBigIntegerExact
+  //       false
+  //     }
+  //     catch {
+  //       case _ => true
+  //     }
+  //   } suchThat {
+  //     case JDecimal(bd) => try {
+  //       BigDecimal(bd.toString)
+  //       true
+  //     }
+  //     catch {
+  //       case _ => false
+  //     }
+  //   }
+  // }
+  def genJDecimal: Gen[JDecimal] = for {
+    d <- arbitrary[Double]
+  } yield JDecimal(BigDecimal(d))
+  def arbJDecimal: Arbitrary[JDecimal] = Arbitrary(genJDecimal)
+
+  def genJDouble: Gen[JDouble] = for {
+    d <- arbitrary[Double]
+  } yield JDouble(d)
+  def arbJDouble: Arbitrary[JDouble] = Arbitrary(genJDouble)
+
+  def genJString: Gen[JString] = for {
+    s <- arbitrary[String]
+  } yield JString(s)
+  def arbJString: Arbitrary[JString] = Arbitrary(genJString)
+
+  def genJBool: Gen[JBool] = for {
+    b <- arbitrary[Boolean]
+  } yield JBool(b)
+  def arbJBool: Arbitrary[JBool] = Arbitrary(genJBool)
+
+  def genJInt: Gen[JInt] = for {
+    l <- arbitrary[Long]
+  } yield JInt(l)
+  def arbJInt: Arbitrary[JInt] = Arbitrary(genJInt)
+
+  def genJNull: Gen[JNull.type] = Gen value JNull
+  def arbJNull: Arbitrary[JNull.type] = Arbitrary(genJNull)
+
+  def genJNothing: Gen[JNothing.type] = Gen value JNothing
+  def arbJNothing: Arbitrary[JNothing.type] = Arbitrary(genJNothing)
+
+  class Nodes(decimalMode: Boolean) {
+
+    implicit val arb = Arbitrary(genJValue)
+    private[this] implicit val arbObj = arbGenJObject
+
+    def genJValue: Gen[JValue] = Gen frequency (
+      (75, genJValueSansFloat),
+      (25, if (decimalMode) genJDecimal else genJDouble)
+    )
+
+    def genJValueSansFloat: Gen[JValue] = Gen frequency (
+      (15, genJNull),
+      (15, genJString),
+      (15, genJInt),
+      (15, genJBool),
+      (30, genJObject),
+      (30, genJArray)
+    )
+
+    def genJField: Gen[JField] = for {
+      s <- arbitrary[String]
+      v <- arbitrary[JValue]
+    } yield (s, v)
+    def arbGenJField: Arbitrary[JField] = Arbitrary(genJField)
+
+    def genJObject: Gen[JObject] = for {
+      lst: List[JField] <- containerOfN[List, JField](MaxSize, genJField)
+    } yield JObject(lst)
+    def arbGenJObject: Arbitrary[JObject] = Arbitrary(genJObject)
+
+    def genJArray: Gen[JArray] = for {
+      obj: JObject <- arbitrary[JObject]
+    } yield JArray {
+      val jvals: List[JValue] = obj.fold(List.empty[JValue]) {
+        (acc, el) => el :: acc
+      }
+      jvals
+    }
+    def arbGenJArray: Arbitrary[JArray] = Arbitrary(genJArray)
+  }
+}
