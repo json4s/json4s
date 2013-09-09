0bd53b6a98709f34394c404228abb216be884616
Trond Marius Øvstetun <trond@ovstetun.no>
2013-09-09 10:17:39 +0200

    Make validation work
    Add type for validation that works with scalaz7 and scala 2.10.
    Fixes #39.

--- scalaz/src/main/scala/org/json4s/scalaz/JsonScalaz.scala
+++ scalaz/src/main/scala/org/json4s/scalaz/JsonScalaz.scala
@@ -67,7 +67,10 @@ trait Types {
     case x => UnexpectedJSONError(x, classOf[JObject]).failureNel
   }
 
-  def validate[A <: JValue : JSONR](name: String): Kleisli[Result, JValue, A] = Kleisli.kleisli(field[A](name))
+  type EitherNel[+a] = NonEmptyList[Error] \/ a
+  def validate[A: JSONR](name: String) = Kleisli(field[A](name)).mapK[EitherNel, A](_.disjunction)
+  implicit def function2EitherNel[A](f: A => Result[A]): (A => EitherNel[A]) = (a: A) => f(a).disjunction
+  implicit def kleisli2Result[A](v: Kleisli[EitherNel, JValue, A]): JValue => Result[A] = (v.run _).andThen(_.validation)
 
   def makeObj(fields: Traversable[(String, JValue)]): JObject = 
     JObject(fields.toList.map { case (n, v) => JField(n, v) })
