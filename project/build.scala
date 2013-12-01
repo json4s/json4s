2426c5ca184c5dc710ecddc073603ab33ed61e58
Ivan Porto Carrero <ivan@flanders.co.nz>
2013-11-30 23:11:49 -0800

    bump to snapshot again

--- project/build.scala
+++ project/build.scala
@@ -56,7 +56,7 @@ object build extends Build {
     scalaVersion := "2.10.0",
     crossScalaVersions := Seq("2.10.0"),
     scalacOptions ++= Seq("-unchecked", "-deprecation", "-optimize", "-feature", "-Yinline-warnings", "-language:existentials", "-language:implicitConversions", "-language:higherKinds", "-language:reflectiveCalls", "-language:postfixOps"),
-    version := "3.2.6",
+    version := "3.2.7-SNAPSHOT",
     javacOptions ++= Seq("-target", "1.6", "-source", "1.6"),
     manifestSetting,
     publishSetting,
