bec7cc806cf4ec292dafd73cf82d66cbff88e289
Ivan Porto Carrero <ivan@flanders.co.nz>
2014-05-13 10:14:25 -0700

    version 3.2.10

--- project/build.scala
+++ project/build.scala
@@ -57,7 +57,7 @@ object build extends Build {
     scalaVersion := "2.9.3",
     crossScalaVersions := Seq("2.9.1", "2.9.1-1", "2.9.2", "2.9.3"),
     scalacOptions ++= Seq("-unchecked", "-deprecation", "-optimize"),
-    version := "3.2.10-SNAPSHOT",
+    version := "3.2.10",
     javacOptions ++= Seq("-target", "1.6", "-source", "1.6"),
     manifestSetting,
     publishSetting,
