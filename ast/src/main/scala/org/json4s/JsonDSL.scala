59355f589baec5ced3809c44492ed1a582a549c2
Ivan Porto Carrero <ivan@flanders.co.nz>
2014-10-23 16:26:25 -0700

    release 3.2.11

--- ast/src/main/scala/org/json4s/JsonDSL.scala
+++ ast/src/main/scala/org/json4s/JsonDSL.scala
@@ -92,15 +92,15 @@ trait JsonDSL extends Implicits {
       val l: JValue = left._2
       JObject(JField(left._1, l) :: right.obj)
     }
-    def ~~[B <% JValue](right: (String, B)) = ~(right)
-    def ~~(right: JObject) = ~(right)
+    def ~~[B <% JValue](right: (String, B)) = this.~(right)
+    def ~~(right: JObject) = this.~(right)
 
   }
 
   class JsonListAssoc(left: List[JField]) {
     def ~(right: (String, JValue)) = JObject(left ::: List(JField(right._1, right._2)))
     def ~(right: JObject) = JObject(left ::: right.obj)
-    def ~~(right: (String, JValue)) = ~(right)
-    def ~~(right: JObject) = ~(right)
+    def ~~(right: (String, JValue)) = this.~(right)
+    def ~~(right: JObject) = this.~(right)
   }
 }
