5bec303db29b0e945eba3522edd2751c1bca976c
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2014-11-19 01:26:58 +0100

    Fixed examples.

--- examples/src/main/scala/org/json4s/examples/MongoExamples.scala
+++ examples/src/main/scala/org/json4s/examples/MongoExamples.scala
@@ -2,7 +2,7 @@ package org.json4s
 package examples
 
 import com.ning.http.client.Response
-import dispatch._
+import dispatch._, Defaults._
 import com.mongodb._
 import mongo.JObjectParser
 
@@ -20,19 +20,30 @@ object MongoExamples extends App with jackson.JsonMethods {
   val mongo = com.mongodb.Mongo.connect(new DBAddress("127.0.0.1", "json4s_examples"))
   val coll = mongo.getCollection("swagger_data")
 
-  val listing = Http(url("http://petstore.swagger.wordnik.com/api/resources.json") OK read.Json)()
-  println("Listing received, storing...")
-  coll.save(JObjectParser.parse(listing))
+  val f = for {
+    listing <- Http(url("http://petstore.swagger.wordnik.com/api/api-docs") OK read.Json)
+    api <- Http(url("http://petstore.swagger.wordnik.com/api/api-docs/pet") OK read.Json)
+  } yield {
+    println("Listing received, storing...")
+    coll.save(JObjectParser.parse(listing))
 
-  val api = Http(url("http://petstore.swagger.wordnik.com/api/pet.json") OK read.Json)()
-  println("Api description received, storing...")
-  coll save {
-    JObjectParser parse {
-      api transformField {
-        case JField(nm, v) if nm.startsWith("$") => JField(s"#${nm.substring(1)}", v)
+    println("Api description received, storing...")
+    coll save {
+      JObjectParser parse {
+        api transformField {
+          case JField(nm, v) if nm.startsWith("$") => JField(s"#${nm.substring(1)}", v)
+        }
       }
     }
+
+    println("Swagger api has been harvested.")
   }
-  println("Swagger api has been harvested.")
-  Http.shutdown()
+
+  f onFailure { case r =>
+    println(r)
+  }
+
+  f onComplete { r =>
+    Http.shutdown()
+  }
 }
