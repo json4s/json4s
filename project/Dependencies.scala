551bdaf60f0b75dd8dcd0d8cee1886c6623990a9
Kazuhiro Sera <seratch@gmail.com>
2015-01-11 16:58:24 +0900

    source-code compatible dependencies upgrade, cleanup build settings

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,43 +3,28 @@ import Keys._
 
 object Dependencies {
 
-  /* Steal from lift build */
-  def crossMapped(mappings: (String, String)*): CrossVersion =
-    CrossVersion.binaryMapped(Map(mappings: _*) orElse { case v => v })
-
-  def defaultOrMapped(default: String, alternatives: (String, String)*): String => String =
-    Map(alternatives: _*).withDefaultValue(default)
-
-  type ModuleMap = String => ModuleID
-
-  /* stop stealing */
-
-  lazy val scalaz_core = "org.scalaz" %% "scalaz-core" % "7.0.6"
-
-  val jodaTime = Seq("joda-time" % "joda-time" % "2.3", "org.joda" % "joda-convert" % "1.6")
-
-  val scalacheck = "org.scalacheck" %% "scalacheck" % "1.10.1" % "test"
-
-  lazy val specs = "org.specs2" %% "specs2"      % "2.3.11"  % "test"
-
-  val jackson = Seq("com.fasterxml.jackson.core" % "jackson-databind" % "2.3.1")
-
-  val jacksonScala = "com.fasterxml.jackson.module" % "jackson-module-scala" % "2.3.1" cross crossMapped("2.9.0" -> "2.9.1", "2.9.0-1" -> "2.9.1", "2.9.1-1" -> "2.9.1")
-
-  val paranamer = "com.thoughtworks.paranamer" % "paranamer" % "2.6"
-
-  val commonsCodec = "commons-codec"              % "commons-codec"      % "1.9"
-
-  val mockito = "org.mockito"                 % "mockito-all"              % "1.9.5"      % "test"
-
-  val liftCommon = "net.liftweb" %% "lift-common" % "2.5.1" cross crossMapped("2.9.3" -> "2.9.2")
+  lazy val jodaTime     = Seq(
+    "joda-time" % "joda-time"    % "2.6", 
+    "org.joda"  % "joda-convert" % "1.7"
+  )
+  lazy val jackson      = Seq(
+    // TODO 2.5 has breaking API changes
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.3.4"
+  )
+  lazy val jacksonScala = "com.fasterxml.jackson.module" %  "jackson-module-scala" % "2.3.1" 
+  lazy val liftCommon   = "net.liftweb"                  %% "lift-common"          % "2.5.1" 
+  // TODO: 7.1 has breaking API changes
+  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.0.6"
+  lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.7"
+  lazy val commonsCodec = "commons-codec"                %  "commons-codec"        % "1.9"
+  // TODO: combination of specs2 2.4 and scalacheck 1.11 has breaking API changes
+  lazy val scalacheck   = "org.scalacheck"               %% "scalacheck"           % "1.10.1"    % "test"
+  lazy val specs        = "org.specs2"                   %% "specs2"               % "2.3.13"    % "test"
+  lazy val mockito      = "org.mockito"                  %  "mockito-all"          % "1.10.19"   % "test"
 
   def scalaXml(scalaVersion: String) = {
-    if(scalaVersion.startsWith("2.11"))
-      Seq("org.scala-lang.modules" %% "scala-xml" % "1.0.3")
-    else
-      Nil
+    if (scalaVersion.startsWith("2.11")) Seq("org.scala-lang.modules" %% "scala-xml" % "1.0.3")
+    else Nil
   }
 
-//  val scalaj_collection = "org.scalaj" %% "scalaj-collection" % "1.2" cross CVMappingAll
 }
