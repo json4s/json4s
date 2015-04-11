613535dd207f2b36ef703bbcbd80945e5c0c6800
Kazuhiro Sera <seratch@gmail.com>
2015-04-11 14:27:59 +0900

    Bump minor versions of Scala, sbt and dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,14 +9,14 @@ object Dependencies {
   )
   lazy val jackson      = Seq(
     // TODO 2.5 has breaking API changes
-    "com.fasterxml.jackson.core" % "jackson-databind" % "2.3.4"
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.3.5"
   )
-  lazy val jacksonScala = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.4.4"
+  lazy val jacksonScala = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.4.5"
   lazy val liftCommon   = "net.liftweb"                  %% "lift-common"          % "2.5.1" 
-  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.1.0"
+  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.1.1"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.7"
   lazy val commonsCodec = "commons-codec"                %  "commons-codec"        % "1.9"
-  lazy val specs        = "org.specs2"                   %% "specs2-scalacheck"    % "2.4.15"    % "test"
+  lazy val specs        = "org.specs2"                   %% "specs2-scalacheck"    % "2.4.17"    % "test"
   lazy val mockito      = "org.mockito"                  %  "mockito-all"          % "1.10.19"   % "test"
 
   def scalaXml(scalaVersion: String) = {
