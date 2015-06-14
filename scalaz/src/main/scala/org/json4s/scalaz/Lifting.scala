efc4c035081726d216d364aaf814c5bf20ba54fb
xuwei-k <6b656e6a69@gmail.com>
2015-06-14 15:26:10 +0900

    use implicit class. avoid structural typing

--- scalaz/src/main/scala/org/json4s/scalaz/Lifting.scala
+++ scalaz/src/main/scala/org/json4s/scalaz/Lifting.scala
@@ -21,37 +21,37 @@ package scalaz
 import _root_.scalaz._
 
 trait Lifting { this: Types =>
-  implicit def Func2ToJSON[A: JSONR, B: JSONR, R](z: (A, B) => R) = new {
+  implicit class Func2ToJSON[A: JSONR, B: JSONR, R](z: (A, B) => R) {
     def applyJSON(a: JValue => Result[A], b: JValue => Result[B]): JValue => Result[R] = 
       (json: JValue) => Apply[Result].apply2(a(json),b(json))(z)
   }
 
-  implicit def Func3ToJSON[A: JSONR, B: JSONR, C: JSONR, R](z: (A, B, C) => R) = new {
+  implicit class Func3ToJSON[A: JSONR, B: JSONR, C: JSONR, R](z: (A, B, C) => R) {
     def applyJSON(a: JValue => Result[A], b: JValue => Result[B], c: JValue => Result[C]): JValue => Result[R] = 
       (json: JValue) => Apply[Result].apply3(a(json),b(json),c(json))(z)
   }
 
-  implicit def Func4ToJSON[A: JSONR, B: JSONR, C: JSONR, D: JSONR, R](z: (A, B, C, D) => R) = new {
+  implicit class Func4ToJSON[A: JSONR, B: JSONR, C: JSONR, D: JSONR, R](z: (A, B, C, D) => R) {
     def applyJSON(a: JValue => Result[A], b: JValue => Result[B], c: JValue => Result[C], d: JValue => Result[D]): JValue => Result[R] = 
       (json: JValue) => Apply[Result].apply4(a(json),b(json),c(json),d(json))(z)
   }
 
-  implicit def Func5ToJSON[A: JSONR, B: JSONR, C: JSONR, D: JSONR, E: JSONR, R](z: (A, B, C, D, E) => R) = new {
+  implicit class Func5ToJSON[A: JSONR, B: JSONR, C: JSONR, D: JSONR, E: JSONR, R](z: (A, B, C, D, E) => R) {
     def applyJSON(a: JValue => Result[A], b: JValue => Result[B], c: JValue => Result[C], d: JValue => Result[D], e: JValue => Result[E]): JValue => Result[R] = 
       (json: JValue) => Apply[Result].apply5(a(json),b(json),c(json),d(json),e(json))(z)
   }
 
-  implicit def Func6ToJSON[A: JSONR, B: JSONR, C: JSONR, D: JSONR, E: JSONR, F: JSONR, R](z: (A, B, C, D, E, F) => R) = new {
+  implicit class Func6ToJSON[A: JSONR, B: JSONR, C: JSONR, D: JSONR, E: JSONR, F: JSONR, R](z: (A, B, C, D, E, F) => R) {
     def applyJSON(a: JValue => Result[A], b: JValue => Result[B], c: JValue => Result[C], d: JValue => Result[D], e: JValue => Result[E], f: JValue => Result[F]): JValue => Result[R] = 
       (json: JValue) => Apply[Result].apply6(a(json),b(json),c(json),d(json),e(json),f(json))(z)
   }
 
-  implicit def Func7ToJSON[A: JSONR, B: JSONR, C: JSONR, D: JSONR, E: JSONR, F: JSONR, G: JSONR, R](z: (A, B, C, D, E, F, G) => R) = new {
+  implicit class Func7ToJSON[A: JSONR, B: JSONR, C: JSONR, D: JSONR, E: JSONR, F: JSONR, G: JSONR, R](z: (A, B, C, D, E, F, G) => R) {
     def applyJSON(a: JValue => Result[A], b: JValue => Result[B], c: JValue => Result[C], d: JValue => Result[D], e: JValue => Result[E], f: JValue => Result[F], g: JValue => Result[G]): JValue => Result[R] = 
       (json: JValue) => Apply[Result].apply7(a(json),b(json),c(json),d(json),e(json),f(json),g(json))(z)
   }
 
-  implicit def Func8ToJSON[A: JSONR, B: JSONR, C: JSONR, D: JSONR, E: JSONR, F: JSONR, G: JSONR, H: JSONR, R](z: (A, B, C, D, E, F, G, H) => R) = new {
+  implicit class Func8ToJSON[A: JSONR, B: JSONR, C: JSONR, D: JSONR, E: JSONR, F: JSONR, G: JSONR, H: JSONR, R](z: (A, B, C, D, E, F, G, H) => R) {
     def applyJSON(a: JValue => Result[A], b: JValue => Result[B], c: JValue => Result[C], d: JValue => Result[D], e: JValue => Result[E], f: JValue => Result[F], g: JValue => Result[G], h: JValue => Result[H]): JValue => Result[R] = 
       (json: JValue) => Apply[Result].apply8(a(json),b(json),c(json),d(json),e(json),f(json),g(json),h(json))(z)
   }
