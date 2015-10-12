18a8259cea6f2a24826383fd21603d99adbc1b7c
xuwei-k <6b656e6a69@gmail.com>
2015-10-13 03:06:50 +0900

    fix ValidationExample

--- tests/src/test/scala/org/json4s/scalaz/ValidationExample.scala
+++ tests/src/test/scala/org/json4s/scalaz/ValidationExample.scala
@@ -1,7 +1,10 @@
 package org.json4s.scalaz
 
 import scalaz._
-import Scalaz._
+import scalaz.syntax.bind._
+import scalaz.syntax.traverse._
+import scalaz.std.list._
+import scalaz.syntax.validation._
 import JsonScalaz._
 import org.json4s._
 
@@ -38,29 +41,27 @@ object ValidationExample extends Specification {
   // This example shows:
   // * a validation where result depends on more than one value
   // * parse a List with invalid values
-// FIXME enable when 2.8 no longer supported, 2.9 needs: import Validation.Monad._
-/*
   "Range filtering" should {
-    val json = JsonParser.parse(""" [{"s":10,"e":17},{"s":12,"e":13},{"s":11,"e":8}] """)
+    val json = native.JsonParser.parse(""" [{"s":10,"e":17},{"s":12,"e":13},{"s":11,"e":8}] """)
 
-    def ascending: (Int, Int) => Result[(Int, Int)] = (x1: Int, x2: Int) =>
+    val ascending = (x1: Int, x2: Int) => {
       if (x1 > x2) Fail("asc", x1 + " > " + x2) else (x1, x2).success
+    }.disjunction
 
     // Valid range is a range having start <= end
     implicit def rangeJSON: JSONR[Range] = new JSONR[Range] {
       def read(json: JValue) =
-        ((field[Int]("s")(json) |@| field[Int]("e")(json)) apply ascending).join map Range.tupled
+        (((field[Int]("s")(json) |@| field[Int]("e")(json))(ascending)).disjunction.join map Range.tupled).validation
     }
 
     "fail if lists contains invalid ranges" in {
       val r = fromJSON[List[Range]](json)
-      r.fail.toOption.get.list must_== List(UncategorizedError("asc", "11 > 8", Nil))
+      r.swap.toOption.get.list must_== List(UncategorizedError("asc", "11 > 8", Nil))
     }
 
     "optionally return only valid ranges" in {
-      val ranges = json.children.map(fromJSON[Range]).filter(_.isSuccess).sequence[PartialApply1Of2[ValidationNEL, Error]#Apply, Range]
+      val ranges = json.children.map(fromJSON[Range]).filter(_.isSuccess).sequenceU
       ranges must_== Success(List(Range(10, 17), Range(12, 13)))
     }
   }
-  */
 }
