4dfc0a85c3ffc5cfbae3c3f2bb233a43aa19f0ba
xuwei-k <6b656e6a69@gmail.com>
2015-10-14 03:08:55 +0900

    fix LottoExample
    (cherry picked from commit 806009e2183ff90aa85533a3ad9d6778f893339f)

--- tests/src/test/scala/org/json4s/scalaz/LottoExample.scala
+++ tests/src/test/scala/org/json4s/scalaz/LottoExample.scala
@@ -1,7 +1,6 @@
 package org.json4s.scalaz
 
 import scalaz._
-import Scalaz._
 import JsonScalaz._
 import org.json4s.native
 import native.JsonMethods._
@@ -14,26 +13,24 @@ object LottoExample extends Specification {
 
   val json = parse("""{"id":5,"winning-numbers":[2,45,34,23,7,5],"winners":[{"winner-id":23,"numbers":[2,45,34,23,3,5]},{"winner-id":54,"numbers":[52,3,12,11,18,22]}]}""")
 
-  // Lotto line must have exactly 6 numbers
-//  def len(x: Int) = (xs: List[Int]) =>
-//    if (xs.length != x) Fail("len", s"${xs.length} != $x") else xs.success
+  def len(x: Int) = (xs: List[Int]) => {
+    if (xs.length != x) Fail("len", s"${xs.length} != $x") else Success(xs)
+  }.disjunction
 
-  // FIXME enable when 2.8 no longer supported, 2.9 needs: import Validation.Monad._
-/*
-  // Note 'apply _' is not needed on Scala 2.8.1 >=
   implicit def winnerJSON: JSONR[Winner] =
-    Winner.applyJSON(field("winner-id"), validate[List[Int]]("numbers") >=> len(6) apply _)
+    Winner.applyJSON(field[Long]("winner-id"), validate[List[Int]]("numbers") >==> len(6))
 
   implicit def lottoJSON: JSONR[Lotto] =
-    Lotto.applyJSON(field("id")
-                  , validate[List[Int]]("winning-numbers") >=> len(6) apply _
-                  , field("winners")
-                  , field("draw-date"))
+    Lotto.applyJSON(field[Long]("id")
+                  , validate[List[Int]]("winning-numbers") >==> len(6)
+                  , field[List[Winner]]("winners")
+                  , field[Option[String]]("draw-date"))
 
   val winners = List(Winner(23, List(2, 45, 34, 23, 3, 5)), Winner(54, List(52, 3, 12, 11, 18, 22)))
   val lotto = Lotto(5, List(2, 45, 34, 23, 7, 5), winners, None)
 
-  fromJSON[Lotto](json) must_== Success(lotto)
-  */
+  "LottoExample" in {
+    fromJSON[Lotto](json) must_== Success(lotto)
+  }
 }
 
