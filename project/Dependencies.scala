6a12d2fbcd7f774dcdf4c05f60600d6596dd4e69
Kaz Sera <seratch@gmail.com>
2016-02-12 18:12:29 +0900

    Bump minor version of dependencies
     - joda-time 2.8.2 -> 2.9.2
     - jackson 2.6.2 -> 2.6.5
     - scalaz 7.1.4 -> 7.1.7
     - sbt 0.13.9 -> 0.13.10-RC3
     - sbt-mima-plugin 0.1.7 -> 0.1.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,18 +4,21 @@ import Keys._
 object Dependencies {
 
   lazy val jodaTime     = Seq(
-    "joda-time" % "joda-time"    % "2.8.2",
-    "org.joda"  % "joda-convert" % "1.7"
+    "joda-time" % "joda-time"    % "2.9.2",
+    "org.joda"  % "joda-convert" % "1.8.1"
   )
   lazy val jackson      = Seq(
-    "com.fasterxml.jackson.core" % "jackson-databind" % "2.6.2"
+    // TODO: 2.7
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.6.5"
   )
-  lazy val jacksonScala = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.6.1"
-  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.1.4"
+  lazy val jacksonScala = "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.6.3"
+  // TODO: 7.2
+  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.1.7"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val commonsCodec = "commons-codec"                %  "commons-codec"        % "1.9"
-  lazy val specs        = "org.specs2"                   %% "specs2-scalacheck"    % "2.4.17"    % "test"
-  lazy val mockito      = "org.mockito"                  %  "mockito-all"          % "1.10.19"   % "test"
+  // NOTE: keep 2.4 to be compatible with scalaz 7.1
+  lazy val specs        = "org.specs2"                   %% "specs2-scalacheck"    % "2.4.17"  % "test"
+  lazy val mockito      = "org.mockito"                  %  "mockito-all"          % "1.10.19" % "test"
 
   def scalaXml(scalaVersion: String) = {
     PartialFunction.condOpt(CrossVersion.partialVersion(scalaVersion)){
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,10 +1,10 @@
-addSbtPlugin("com.typesafe"     % "sbt-mima-plugin"      % "0.1.7")
+addSbtPlugin("com.typesafe"     % "sbt-mima-plugin"      % "0.1.8")
 addSbtPlugin("com.typesafe.sbt" % "sbt-javaversioncheck" % "0.1.0")
 addSbtPlugin("com.eed3si9n"     % "sbt-buildinfo"        % "0.3.0")
 addSbtPlugin("com.typesafe.sbt" % "sbt-start-script"     % "0.10.0")
-addSbtPlugin("org.xerial.sbt"   % "sbt-sonatype"         % "0.5.0")
+addSbtPlugin("org.xerial.sbt"   % "sbt-sonatype"         % "1.0")
 addSbtPlugin("com.jsuereth"     % "sbt-pgp"              % "1.0.0")
-addSbtPlugin("com.timushev.sbt" % "sbt-updates"          % "0.1.9")
+addSbtPlugin("com.timushev.sbt" % "sbt-updates"          % "0.1.10")
 
 scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
 
