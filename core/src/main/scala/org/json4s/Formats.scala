b727c472fef10b4a44d574a0947ddb37a865a398
Gary Feltham <garyfeltham@gmail.com>
2016-09-15 19:50:13 +0800

    addition of null checks to class1/2 when traits (#343)
    * addition of null checks to class1/2 when traits
    
    * added NPE check for https://github.com/json4s/json4s/issues/342

--- core/src/main/scala/org/json4s/Formats.scala
+++ core/src/main/scala/org/json4s/Formats.scala
@@ -272,6 +272,8 @@ trait Formats extends Serializable { self: Formats =>
   private[json4s] object ClassDelta {
     def delta(class1: Class[_], class2: Class[_]): Int = {
       if (class1 == class2) 0
+      else if (class1 == null) 1
+      else if (class2 == null) -1
       else if (class1.getInterfaces.contains(class2)) 0
       else if (class2.getInterfaces.contains(class1)) 0
       else if (class1.isAssignableFrom(class2)) {
