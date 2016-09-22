30a411b290da788318e432806e2d940f7b8baef5
Kazuhiro Sera <seratch@gmail.com>
2016-09-22 23:06:31 +0900

    version 3.4.1

--- project/build.scala
+++ project/build.scala
@@ -63,7 +63,7 @@ object build extends Build {
           Nil
       }
     },
-    version := "3.4.1-SNAPSHOT",
+    version := "3.4.1",
     javacOptions ++= Seq("-target", "1.6", "-source", "1.6"),
     javaVersionPrefix in javaVersionCheck := Some{
       CrossVersion.partialVersion(scalaVersion.value) match {
---
+++ publish_2.12.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sbt ++2.12.0-RC1 javaVersionCheck publishSigned
