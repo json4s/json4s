c0739579c1972d2e3da68b375a46958141fb538a
kenji yoshida <6b656e6a69@gmail.com>
2016-10-19 00:24:00 +0900

    Scala 2.12.0-RC2 (#416)

--- project/build.scala
+++ project/build.scala
@@ -53,7 +53,7 @@ object build {
   val json4sSettings = mavenCentralFrouFrou ++ Seq(
     organization := "org.json4s",
     scalaVersion := "2.11.8",
-    crossScalaVersions := Seq("2.10.6", "2.11.8", "2.12.0-RC1"),
+    crossScalaVersions := Seq("2.10.6", "2.11.8", "2.12.0-RC2"),
     scalacOptions ++= Seq("-unchecked", "-deprecation", "-optimize", "-feature", "-language:existentials", "-language:implicitConversions", "-language:higherKinds", "-language:postfixOps"),
     scalacOptions ++= {
       CrossVersion.partialVersion(scalaVersion.value) match {
