a00cf81aab76a8b0f6c86c50122fe66d6a42018f
Kazuhiro Sera <seratch@gmail.com>
2016-10-19 01:45:04 +0900

    Start 3.4.3-SNAPSHOT

--- project/build.scala
+++ project/build.scala
@@ -63,7 +63,7 @@ object build extends Build {
           Nil
       }
     },
-    version := "3.4.2",
+    version := "3.4.3-SNAPSHOT",
     javacOptions ++= Seq("-target", "1.6", "-source", "1.6"),
     javaVersionPrefix in javaVersionCheck := Some{
       CrossVersion.partialVersion(scalaVersion.value) match {
