717a2e45279afc469f9214fbba655a5448cf117e
Kazuhiro Sera <seratch@gmail.com>
2017-03-18 13:30:15 +0900

    version 3.5.1

--- project/build.scala
+++ project/build.scala
@@ -70,7 +70,7 @@ object build {
           Nil
       }
     },
-    version := "3.5.1-SNAPSHOT",
+    version := "3.5.1",
     javacOptions ++= Seq("-target", "1.6", "-source", "1.6"),
     javaVersionPrefix in javaVersionCheck := Some{
       CrossVersion.partialVersion(scalaVersion.value) match {
