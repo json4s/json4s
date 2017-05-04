b57d8e793308e4749a1b9a80b7f33c75a9bfb14f
xuwei-k <6b656e6a69@gmail.com>
2017-05-05 05:41:10 +0900

    3.5.3-SNAPSHOT

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -4,14 +4,17 @@ import com.typesafe.tools.mima.plugin.MimaKeys._
 
 object MimaSettings {
 
-  val previousVersions = Set(0, 1).map(patch => s"3.5.$patch")
+  val previousVersions = Set(0, 1, 2).map(patch => s"3.5.$patch")
 
   val mimaSettings = MimaPlugin.mimaDefaultSettings ++ Seq(
     mimaPreviousArtifacts := {
       CrossVersion.partialVersion(scalaVersion.value) match {
         case Some((2, scalaMajor)) if scalaMajor <= 12 =>
           previousVersions.map { organization.value % s"${name.value}_${scalaBinaryVersion.value}" % _ }
-        case _ => Set.empty
+        case _ =>
+          Set(2).map(patch => s"3.5.$patch").map {
+            organization.value % s"${name.value}_${scalaBinaryVersion.value}" % _
+          }
       }
     },
     test in Test := {
