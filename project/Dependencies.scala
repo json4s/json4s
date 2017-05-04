14b4a707ce62f1dcc0cabf53447e9a0a4d1924b3
xuwei-k <6b656e6a69@gmail.com>
2017-05-05 05:14:16 +0900

    Scala 2.13.0-M1
    temporarily disable test dependency. check compile only

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,12 @@ object Dependencies {
   lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.12"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val specs        = Def.setting{
-    Seq("org.specs2" %% "specs2-scalacheck" % "3.8.9" % "test")
+    // TODO enable if specs2 for Scala 2.13 released
+    // https://github.com/etorreborre/specs2/issues/573
+    PartialFunction.condOpt(CrossVersion.partialVersion(scalaVersion.value)){
+      case Some((2, v)) if v <= 12 =>
+        Seq("org.specs2" %% "specs2-scalacheck" % "3.8.9" % "test")
+    }.toList.flatten
   }
   lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.7.19" % "test"
 
