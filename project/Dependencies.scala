f22831191f172146c54b515dfae019380d14ee7d
xuwei-k <6b656e6a69@gmail.com>
2017-07-29 08:50:12 +0900

    Scala 2.13.0-M2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,11 @@ object Dependencies {
   lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.14"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val specs        = Def.setting{
-    Seq("org.specs2" %% "specs2-scalacheck" % "3.9.0" % "test")
+    // TODO enable if specs2 for Scala 2.13.0-M2 released
+    PartialFunction.condOpt(CrossVersion.partialVersion(scalaVersion.value)){
+      case Some((2, v)) if v <= 12 =>
+        Seq("org.specs2" %% "specs2-scalacheck" % "3.8.9" % "test")
+    }.toList.flatten
   }
   lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.7.22" % "test"
 
