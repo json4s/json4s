19471d4c9349ead1e85b0f2def45e1870b648a50
xuwei-k <6b656e6a69@gmail.com>
2017-10-14 00:20:25 +0900

    3.6.0-SNAPSHOT

--- project/build.scala
+++ project/build.scala
@@ -72,7 +72,7 @@ object build {
           Nil
       }
     },
-    version := "3.6.0-M1",
+    version := "3.6.0-SNAPSHOT",
     javacOptions ++= Seq("-target", "1.8", "-source", "1.8"),
     javaVersionPrefix in javaVersionCheck := Some("1.8"),
     parallelExecution in Test := false,
