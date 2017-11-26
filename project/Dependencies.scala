38aba7e82e9852e30324cf88b421820192cd8aa4
xuwei-k <6b656e6a69@gmail.com>
2017-11-26 21:58:33 +0900

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,12 +5,12 @@ object Dependencies {
 
   lazy val jodaTime     = Seq(
     "joda-time" % "joda-time"    % "2.9.9",
-    "org.joda"  % "joda-convert" % "1.8.2"
+    "org.joda"  % "joda-convert" % "1.9.2"
   )
   lazy val jackson      = Seq(
-    "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.0"
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.2"
   )
-  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.14"
+  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.17"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val specs        = Def.setting{
     // TODO enable if specs2 for Scala 2.13.0-M2 released
@@ -19,7 +19,7 @@ object Dependencies {
         Seq("org.specs2" %% "specs2-scalacheck" % "3.9.4" % "test")
     }.toList.flatten
   }
-  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.8.47" % "test"
+  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.12.0" % "test"
 
   def scalaXml(scalaVersion: String) = {
     PartialFunction.condOpt(CrossVersion.partialVersion(scalaVersion)){
