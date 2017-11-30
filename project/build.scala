86ad3a71ac8fce2fb424d25d1bb5116636d464b9
kenji yoshida <6b656e6a69@gmail.com>
2017-11-30 22:46:06 +0900

    SNAPSHOT

--- project/build.scala
+++ project/build.scala
@@ -76,7 +76,7 @@ object build {
           Nil
       }
     },
-    version := "3.6.0-M2",
+    version := "3.6.0-SNAPSHOT",
     javacOptions ++= Seq("-target", "1.8", "-source", "1.8"),
     parallelExecution in Test := false,
     manifestSetting,
