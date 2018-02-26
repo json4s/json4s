bfcb01637160f820c85da9e033100ee163044c21
kenji yoshida <6b656e6a69@gmail.com>
2018-02-27 05:31:32 +0900

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,22 +10,22 @@ object Dependencies {
   lazy val jackson      = Seq(
     "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.4"
   )
-  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.19"
+  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.20"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val specs        = Def.setting{
     CrossVersion.partialVersion(scalaVersion.value) match {
       case Some((2, v)) if v <= 10 =>
         Seq("org.specs2" %% "specs2-scalacheck" % "3.9.4" % "test")
       case _ =>
-        Seq("org.specs2" %% "specs2-scalacheck" % "4.0.2" % "test")
+        Seq("org.specs2" %% "specs2-scalacheck" % "4.0.3" % "test")
     }
   }
-  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.13.0" % "test"
+  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.15.0" % "test"
 
   def scalaXml(scalaVersion: String) = {
     PartialFunction.condOpt(CrossVersion.partialVersion(scalaVersion)){
       case Some((2, scalaMajor)) if scalaMajor >= 11 =>
-        Seq("org.scala-lang.modules" %% "scala-xml" % "1.0.6")
+        Seq("org.scala-lang.modules" %% "scala-xml" % "1.1.0")
     }.toList.flatten
   }
 
