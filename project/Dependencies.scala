0a80406e540b894c0560b46bd2575e73431464de
kenji yoshida <6b656e6a69@gmail.com>
2018-04-29 08:43:53 +0900

    scalaz 7.2.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   lazy val jackson      = Seq(
     "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.4"
   )
-  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.20"
+  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.22"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val specs        = Def.setting{
     CrossVersion.partialVersion(scalaVersion.value) match {
