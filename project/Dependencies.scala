dc184e876929856784d43fdfe3de331a5023a8b5
Kazuhiro Sera <seratch@gmail.com>
2018-07-19 13:37:50 +0900

    version 3.6.0-RC1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,13 +5,13 @@ object Dependencies {
   lazy val jaxbApi = "javax.xml.bind" % "jaxb-api" % "2.3.0" % "test"
 
   lazy val jodaTime     = Seq(
-    "joda-time" % "joda-time"    % "2.9.9",
-    "org.joda"  % "joda-convert" % "1.9.2"
+    "joda-time" % "joda-time"    % "2.10",
+    "org.joda"  % "joda-convert" % "2.1.1"
   )
   lazy val jackson      = Seq(
-    "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.5"
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.6"
   )
-  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.23"
+  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.25"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val specs        = Def.setting{
     CrossVersion.partialVersion(scalaVersion.value) match {
@@ -21,7 +21,7 @@ object Dependencies {
         Seq("org.specs2" %% "specs2-scalacheck" % "4.3.2" % "test")
     }
   }
-  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.18.3" % "test"
+  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.19.1" % "test"
 
   def scalaXml(scalaVersion: String) = {
     PartialFunction.condOpt(CrossVersion.partialVersion(scalaVersion)){
