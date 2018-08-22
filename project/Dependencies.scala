8ac4048138d0d5e388b813a7d12a185228ac574f
xuwei-k <6b656e6a69@gmail.com>
2018-08-22 14:58:27 +0900

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,10 +11,10 @@ object Dependencies {
   lazy val jackson      = Seq(
     "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.6"
   )
-  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.25"
+  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.26"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
-  lazy val specs        = "org.specs2"                   %% "specs2-scalacheck"    % "4.3.2" % "test"
-  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.19.1" % "test"
+  lazy val specs        = "org.specs2"                   %% "specs2-scalacheck"    % "4.3.3" % "test"
+  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.21.0" % "test"
 
   lazy val scalaXml     = "org.scala-lang.modules"       %% "scala-xml"            % "1.1.0"
 }
