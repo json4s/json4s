7059c99633cac847e1d0dbe42418e28c1c2de4ba
xuwei-k <6b656e6a69@gmail.com>
2018-08-29 01:29:30 +0900

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   lazy val jackson      = Seq(
     "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.6"
   )
-  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.25"
+  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.26"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val specs        = Def.setting{
     CrossVersion.partialVersion(scalaVersion.value) match {
@@ -21,7 +21,7 @@ object Dependencies {
         Seq("org.specs2" %% "specs2-scalacheck" % "4.3.2" % "test")
     }
   }
-  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.19.1" % "test"
+  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.21.0" % "test"
 
   def scalaXml(scalaVersion: String) = {
     PartialFunction.condOpt(CrossVersion.partialVersion(scalaVersion)){
