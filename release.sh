4fae9222ea7b629186c66eee4e0526175ee67ea5
xuwei-k <6b656e6a69@gmail.com>
2018-10-13 09:39:57 +0900

    Scala 2.13.0-M5
    (cherry picked from commit 2ac4e0cccef07c580d399f02fec364bbd184abee)

--- release.sh
+++ release.sh
@@ -6,12 +6,5 @@ sbt ++2.10.7 \
     ++2.12.7 \
     publishSigned \
     ++2.13.0-M5 \
-    json4s-ast/publishSigned \
-    json4s-core/publishSigned \
-    json4s-ext/publishSigned \
-    json4s-jackson/publishSigned \
-    json4s-mongo/publishSigned \
-    json4s-native/publishSigned \
-    json4s-scalap/publishSigned \
-    json4s-scalaz/publishSigned \
+    publishSigned \
     sonatypeRelease
