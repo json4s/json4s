618eda06e5147eba2da63df3e1a3bd3d20e7af4e
xuwei-k <6b656e6a69@gmail.com>
2018-11-03 05:28:56 +0000

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,19 +2,19 @@ import sbt._
 import Keys._
 
 object Dependencies {
-  lazy val jaxbApi = "javax.xml.bind" % "jaxb-api" % "2.3.0" % "test"
+  lazy val jaxbApi = "javax.xml.bind" % "jaxb-api" % "2.3.1" % "test"
 
   lazy val jodaTime     = Seq(
-    "joda-time" % "joda-time"    % "2.10",
-    "org.joda"  % "joda-convert" % "2.1.1"
+    "joda-time" % "joda-time"    % "2.10.1",
+    "org.joda"  % "joda-convert" % "2.1.2"
   )
   lazy val jackson      = Seq(
-    "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.6"
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.7"
   )
   lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.26"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val specs        = "org.specs2"                   %% "specs2-scalacheck"    % "4.3.5" % "test"
-  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.21.0" % "test"
+  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.23.0" % "test"
 
-  lazy val scalaXml     = "org.scala-lang.modules"       %% "scala-xml"            % "1.1.0"
+  lazy val scalaXml     = "org.scala-lang.modules"       %% "scala-xml"            % "1.1.1"
 }
