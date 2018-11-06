51b749c934878cc53c796ef89938339f1bbf9ee4
kenji yoshida <6b656e6a69@gmail.com>
2018-11-06 10:01:50 +0900

    scalaz 7.2.27

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   lazy val jackson      = Seq(
     "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.7"
   )
-  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.26"
+  lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.27"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val specs        = "org.specs2"                   %% "specs2-scalacheck"    % "4.3.5" % "test"
   lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.23.0" % "test"
