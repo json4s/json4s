1fa7ed5ae5ee77c9bf15d32cc01df4811d3cec89
xuwei-k <6b656e6a69@gmail.com>
2019-01-17 01:05:23 +0000

    Update joda-convert to 2.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   lazy val jodaTime     = Seq(
     "joda-time" % "joda-time"    % "2.10.1",
-    "org.joda"  % "joda-convert" % "2.1.2"
+    "org.joda"  % "joda-convert" % "2.2.0"
   )
   lazy val jackson      = Seq(
     "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.8"
