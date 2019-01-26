a5a34e8a1450a9e9c8c7699261da2084f1bffdfa
Kazuhiro Sera <seratch@gmail.com>
2019-01-26 22:25:07 +0900

    version 3.6.4

--- project/build.scala
+++ project/build.scala
@@ -76,7 +76,7 @@ object build {
           Nil
       }
     },
-    version := "3.6.4-SNAPSHOT",
+    version := "3.6.4",
     javacOptions ++= Seq("-target", "1.8", "-source", "1.8"),
     Seq(Compile, Test).map { scope =>
       unmanagedSourceDirectories in scope += {
