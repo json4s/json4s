8ebed275e983b9c1875411fcdc335465ef4ced01
kenji yoshida <6b656e6a69@gmail.com>
2019-02-02 14:31:38 +0900

    specs2 4.4.1, mockito 2.24.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,8 +13,8 @@ object Dependencies {
   )
   lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.27"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
-  lazy val specs        = "org.specs2"                   %% "specs2-scalacheck"    % "4.4.0" % "test"
-  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.23.4" % "test"
+  lazy val specs        = "org.specs2"                   %% "specs2-scalacheck"    % "4.4.1" % "test"
+  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.24.0" % "test"
 
   lazy val scalaXml     = "org.scala-lang.modules"       %% "scala-xml"            % "1.1.1"
 }
