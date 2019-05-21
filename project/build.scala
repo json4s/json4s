f3eaa78f546cfb4576024d85488f233de69f6cbb
xuwei-k <6b656e6a69@gmail.com>
2019-05-21 17:04:25 +0900

    enable Scala 2.13.0-RC2 test
    (cherry picked from commit e3a6ba09eb5975b389952316d297ee9f9ba7564d)

--- project/build.scala
+++ project/build.scala
@@ -58,7 +58,7 @@ object build {
   val json4sSettings = mavenCentralFrouFrou ++ mimaSettings ++ Def.settings(
     organization := "org.json4s",
     scalaVersion := "2.12.8",
-    crossScalaVersions := Seq("2.10.7", "2.11.12", "2.12.8", "2.13.0-RC1"),
+    crossScalaVersions := Seq("2.10.7", "2.11.12", "2.12.8", "2.13.0-RC2"),
     scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-language:existentials", "-language:implicitConversions", "-language:higherKinds", "-language:postfixOps", "-Xfuture"),
     scalacOptions ++= PartialFunction.condOpt(CrossVersion.partialVersion(scalaVersion.value)) {
       case Some((2, 10)) => "-optimize"
