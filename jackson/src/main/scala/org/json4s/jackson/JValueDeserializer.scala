c93a4af9e3463339fafac77490f3c3220446314f
Magnolia.K <magnolia.k@icloud.com>
2019-06-16 15:05:35 +0900

    fix Json4sScalaModule deserialisation logic
    https://github.com/json4s/json4s/issues/603
    
    Fixed the problem that deserialization was not correctly done
    when passing something other than JValue to the readValue method
    (eg JObject).

--- jackson/src/main/scala/org/json4s/jackson/JValueDeserializer.scala
+++ jackson/src/main/scala/org/json4s/jackson/JValueDeserializer.scala
@@ -10,45 +10,50 @@ import scala.annotation.switch
 class JValueDeserializer(klass: Class[_]) extends JsonDeserializer[Object] {
   def deserialize(jp: JsonParser, ctxt: DeserializationContext): Object = {
 
-    if (jp.getCurrentToken == null) jp.nextToken()
+    def _deserialize(jp: JsonParser, ctxt: DeserializationContext): Object = {
 
-    val value = (jp.getCurrentToken.id(): @switch) match {
-      case JsonTokenId.ID_NULL => JNull
-      case JsonTokenId.ID_NUMBER_INT =>
-        if (ctxt.isEnabled(DeserializationFeature.USE_BIG_INTEGER_FOR_INTS)) JInt(BigInt(jp.getText))
-        else JLong(jp.getLongValue)
-      case JsonTokenId.ID_NUMBER_FLOAT =>
-        if (ctxt.isEnabled(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS)) JDecimal(BigDecimal(jp.getDecimalValue))
-        else JDouble(jp.getDoubleValue)
-      case JsonTokenId.ID_STRING => JString(jp.getText)
-      case JsonTokenId.ID_TRUE => JBool.True
-      case JsonTokenId.ID_FALSE => JBool.False
+      if (jp.getCurrentToken == null) jp.nextToken()
 
-      case JsonTokenId.ID_START_ARRAY =>
-        val values = new mutable.ListBuffer[JValue]()
-        jp.nextToken()
-        while(jp.getCurrentToken != JsonToken.END_ARRAY) {
-          values += deserialize(jp, ctxt).asInstanceOf[JValue]
+      (jp.getCurrentToken.id(): @switch) match {
+        case JsonTokenId.ID_NULL => JNull
+        case JsonTokenId.ID_NUMBER_INT =>
+          if (ctxt.isEnabled(DeserializationFeature.USE_BIG_INTEGER_FOR_INTS)) JInt(BigInt(jp.getText))
+          else JLong(jp.getLongValue)
+        case JsonTokenId.ID_NUMBER_FLOAT =>
+          if (ctxt.isEnabled(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS)) JDecimal(BigDecimal(jp.getDecimalValue))
+          else JDouble(jp.getDoubleValue)
+        case JsonTokenId.ID_STRING => JString(jp.getText)
+        case JsonTokenId.ID_TRUE => JBool.True
+        case JsonTokenId.ID_FALSE => JBool.False
+
+        case JsonTokenId.ID_START_ARRAY =>
+          val values = new mutable.ListBuffer[JValue]()
           jp.nextToken()
-        }
-        JArray(values.toList)
+          while(jp.getCurrentToken != JsonToken.END_ARRAY) {
+            values += _deserialize(jp, ctxt).asInstanceOf[JValue]
+            jp.nextToken()
+          }
+          JArray(values.toList)
 
-      case JsonTokenId.ID_START_OBJECT =>
-        jp.nextToken()
-        deserialize(jp, ctxt)
-
-      case JsonTokenId.ID_FIELD_NAME | JsonTokenId.ID_END_OBJECT =>
-        val fields = new mutable.ListBuffer[JField]
-        while (jp.getCurrentToken.id() != JsonTokenId.ID_END_OBJECT) {
-          val name = jp.getCurrentName
+        case JsonTokenId.ID_START_OBJECT =>
           jp.nextToken()
-          fields += JField(name, deserialize(jp, ctxt).asInstanceOf[JValue])
-          jp.nextToken()
-        }
-        JObject(fields.toList)
+          _deserialize(jp, ctxt)
 
-      case _ => ctxt.handleUnexpectedToken(classOf[JValue], jp)
+        case JsonTokenId.ID_FIELD_NAME | JsonTokenId.ID_END_OBJECT =>
+          val fields = new mutable.ListBuffer[JField]
+          while (jp.getCurrentToken.id() != JsonTokenId.ID_END_OBJECT) {
+            val name = jp.getCurrentName
+            jp.nextToken()
+            fields += JField(name, _deserialize(jp, ctxt).asInstanceOf[JValue])
+            jp.nextToken()
+          }
+          JObject(fields.toList)
+
+        case _ => ctxt.handleUnexpectedToken(classOf[JValue], jp)
+      }
     }
+
+    val value = _deserialize(jp, ctxt)
 
     if (!klass.isAssignableFrom(value.getClass)) ctxt.handleUnexpectedToken(klass, jp)
 
