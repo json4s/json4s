d0423753de0db34f5c57d44662057a79f20e5696
Magnolia.K <magnolia.k@icloud.com>
2019-06-16 23:37:18 +0900

    Added tests for Jackson deserialisation logic

---
+++ tests/src/test/scala/org/json4s/JacksonDeserializationSpec.scala
@@ -0,0 +1,33 @@
+package org.json4s
+
+import org.specs2.mutable.Specification
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.json4s.jackson.Json4sScalaModule
+import org.json4s._
+
+// fix https://github.com/json4s/json4s/issues/603
+class JacksonDeserializationSpec extends Specification {
+  val mapper = new ObjectMapper
+  mapper.registerModule(Json4sScalaModule)
+
+  "Recursive deserialization" should {
+
+    "create JObject by assigning to 'JValue'" in {
+      mapper.readValue("""{"x":"y"}""", classOf[JValue]) must_== JObject(List(("x",JString("y"))))
+    }
+
+    "create JObject by assigning to 'JObject'" in {
+      mapper.readValue("""{"x":"y"}""", classOf[JObject]) must_== JObject(List(("x",JString("y"))))
+    }
+
+    "create JArray by assigning to 'JValue'" in {
+      mapper.readValue("""["x", "y"]""", classOf[JValue]) must_== JArray(List(JString("x"), JString("y")))
+    }
+
+    "create JArray by assigning to 'JArray'" in {
+      mapper.readValue("""["x", "y"]""", classOf[JArray]) must_== JArray(List(JString("x"), JString("y")))
+    }
+  }
+}
+
