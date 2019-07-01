866f55e4f3f9847634cdf304bcdc190b052e52d2
kenji yoshida <6b656e6a69@gmail.com>
2019-07-01 12:08:55 +0900

    Update specs2 to 4.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.28"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val specs        = Def.setting(
-    "org.specs2" %% "specs2-scalacheck"            % "4.5.1" % "test"
+    "org.specs2" %% "specs2-scalacheck"            % "4.6.0" % "test"
   )
   lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "2.28.2" % "test"
 
