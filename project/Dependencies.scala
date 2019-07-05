8d60c087c4809de555c00b7663f8f233bff7ff33
xuwei-k <6b656e6a69@gmail.com>
2019-07-05 01:05:58 +0000

    Update joda-time to 2.10.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val jaxbApi = "javax.xml.bind" % "jaxb-api" % "2.3.1" % "test"
 
   lazy val jodaTime     = Seq(
-    "joda-time" % "joda-time"    % "2.10.2",
+    "joda-time" % "joda-time"    % "2.10.3",
     "org.joda"  % "joda-convert" % "2.2.1"
   )
   lazy val jackson      = Seq(
