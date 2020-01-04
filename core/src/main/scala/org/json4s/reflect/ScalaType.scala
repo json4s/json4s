566a999fcf058309a63c46774e86253c7c50095e
Aris Santas <asantas93@gmail.com>
2020-01-04 16:10:13 -0500

    Make ScalaType manifest public

--- core/src/main/scala/org/json4s/reflect/ScalaType.scala
+++ core/src/main/scala/org/json4s/reflect/ScalaType.scala
@@ -89,7 +89,7 @@ object ScalaType {
   /* end optimization */
 }
 
-class ScalaType(private[json4s] val manifest: Manifest[_]) extends Equals {
+class ScalaType(val manifest: Manifest[_]) extends Equals {
 
   import ScalaType.{ types, CopiedScalaType }
   val erasure: Class[_] = manifest.runtimeClass
