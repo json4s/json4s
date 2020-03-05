a1b25b40341e205bc222b6948c7c184129a08979
kenji yoshida <6b656e6a69@gmail.com>
2020-03-05 11:41:18 +0900

    Update jackson to 2.10.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     "org.joda"  % "joda-convert" % "2.2.1"
   )
   lazy val jackson      = Seq(
-    "com.fasterxml.jackson.core" % "jackson-databind" % "2.10.2"
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.10.3"
   )
   lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.2.30"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
