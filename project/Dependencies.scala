d0887affbcce2f34b301d2d25502099d310869cd
kenji yoshida <6b656e6a69@gmail.com>
2020-03-17 09:35:16 +0900

    scala-xml 1.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,6 +19,6 @@ object Dependencies {
   lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "3.3.3" % "test"
 
   lazy val scalaXml     = Def.setting {
-    "org.scala-lang.modules" %% "scala-xml" % "1.2.0"
+    "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
   }
 }
