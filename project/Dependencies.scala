5f397ec0c66c11dc56080fee9f572abbb88dbdb1
kenji yoshida <6b656e6a69@gmail.com>
2020-08-03 13:35:19 +0900

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,14 +9,14 @@ object Dependencies {
     "org.joda"  % "joda-convert" % "2.2.1"
   )
   lazy val jackson      = Seq(
-    "com.fasterxml.jackson.core" % "jackson-databind" % "2.11.1"
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.11.2"
   )
   lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.3.2"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val specs        = Def.setting(
     "org.specs2" %% "specs2-scalacheck"            % "4.10.1" % "test"
   )
-  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "3.4.4" % "test"
+  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "3.4.6" % "test"
 
   lazy val scalaXml     = Def.setting {
     "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
