6fddb5c8b3a5f0f902515fb84bfadbba0d4d45c7
kenji yoshida <6b656e6a69@gmail.com>
2020-08-16 14:00:24 +0900

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,9 +14,9 @@ object Dependencies {
   lazy val scalaz_core  = "org.scalaz"                   %% "scalaz-core"          % "7.3.2"
   lazy val paranamer    = "com.thoughtworks.paranamer"   %  "paranamer"            % "2.8"
   lazy val specs        = Def.setting(
-    "org.specs2" %% "specs2-scalacheck"            % "4.10.2" % "test"
+    "org.specs2" %% "specs2-scalacheck"            % "4.10.3" % "test"
   )
-  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "3.4.6" % "test"
+  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "3.5.0" % "test"
 
   lazy val scalaXml     = Def.setting {
     "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
