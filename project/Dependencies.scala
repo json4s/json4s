0eb8dd1f3ec25964618f9587b3e5eb16c9a33d7e
kenji yoshida <6b656e6a69@gmail.com>
2020-09-06 16:50:17 +0900

    mockito 3.5.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   lazy val specs        = Def.setting(
     "org.specs2" %% "specs2-scalacheck"            % "4.10.3" % "test"
   )
-  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "3.5.7" % "test"
+  lazy val mockito      = "org.mockito"                  %  "mockito-core"         % "3.5.10" % "test"
 
   lazy val scalaXml     = Def.setting {
     "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
