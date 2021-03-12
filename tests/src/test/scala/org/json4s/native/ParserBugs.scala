546d47c2b9bf2e68d2e38dbcb46cfa8273aaaa1d
kenji yoshida <6b656e6a69@gmail.com>
2021-03-12 23:35:17 +0900

    use scalatest

--- tests/src/test/scala/org/json4s/native/ParserBugs.scala
+++ tests/src/test/scala/org/json4s/native/ParserBugs.scala
@@ -1,41 +1,45 @@
 package org.json4s
 
 import util.control.Exception._
-import org.specs2.mutable.Specification
+import org.scalatest.wordspec.AnyWordSpec
 
-class ParserBugs extends Specification {
+class ParserBugs extends AnyWordSpec {
   import native.JsonParser
   import native.JsonMethods._
 
   "For ParserBugs" should {
     "Unicode ffff is a valid char in string literal" in {
-      parseOpt(""" {"x":"\uffff"} """).isDefined must_== true
+      assert(parseOpt(""" {"x":"\uffff"} """).isDefined)
     }
 
     "Does not hang when parsing 2.2250738585072012e-308" in {
-      allCatch.opt(parse(""" [ 2.2250738585072012e-308 ] """)) must_== None
-      allCatch.opt(parse(""" [ 22.250738585072012e-309 ] """)) must_== None
+      assert(allCatch.opt(parse(""" [ 2.2250738585072012e-308 ] """)).isEmpty)
+      assert(allCatch.opt(parse(""" [ 22.250738585072012e-309 ] """)).isEmpty)
     }
 
     "Does not allow colon at start of array (1039)" in {
-      parseOpt("""[:"foo", "bar"]""") must_== None
+      assert(parseOpt("""[:"foo", "bar"]""").isEmpty)
     }
 
     "Does not allow colon instead of comma in array (1039)" in {
-      parseOpt("""["foo" : "bar"]""") must_== None
+      assert(parseOpt("""["foo" : "bar"]""").isEmpty)
     }
 
     "Solo quote mark should fail cleanly (not StringIndexOutOfBoundsException) (1041)" in {
-      JsonParser.parse("\"", discardParser) must throwA[Throwable].like { case e: ParserUtil.ParseException =>
-        e.getMessage must startWith("unexpected eof")
+      try {
+        JsonParser.parse("\"", discardParser)
+        fail("should be throw ParseException")
+      } catch {
+        case e: ParserUtil.ParseException =>
+          assert(e.getMessage.startsWith("unexpected eof"))
       }
     }
 
     "Field names must be quoted" in {
       val json = JObject(List(JField("foo\nbar", JInt(1))))
       val s = compact(render(json))
-      s must_== """{"foo\nbar":1}"""
-      parse(s) must_== json
+      assert(s == """{"foo\nbar":1}""")
+      assert(parse(s) == json)
     }
   }
 
