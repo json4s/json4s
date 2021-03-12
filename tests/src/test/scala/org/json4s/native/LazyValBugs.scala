f4bf9ddf2fc4e7406e273e477a5f8188361de667
kenji yoshida <6b656e6a69@gmail.com>
2021-03-12 22:44:24 +0900

    use scalatest

--- tests/src/test/scala/org/json4s/native/LazyValBugs.scala
+++ tests/src/test/scala/org/json4s/native/LazyValBugs.scala
@@ -2,7 +2,7 @@ package org.json4s.native
 
 import org.json4s.{FieldSerializer, Formats, DefaultFormats}
 import org.json4s.native.Serialization.{read, write}
-import org.specs2.mutable.Specification
+import org.scalatest.wordspec.AnyWordSpec
 
 class MovieNode(val id: String, val title: String)
 
@@ -18,7 +18,7 @@ class ActorNode(val id: String, val name: String) {
  * Test of a fix for failed lazy val serialization from FieldSerializer
  * where the serialized lazy val will get default value result (null, 0, false, etc) instead of the real value.
  */
-class LazyValBugs extends Specification {
+class LazyValBugs extends AnyWordSpec {
 
   "LazyValBugs Test" should {
 
@@ -28,11 +28,11 @@ class LazyValBugs extends Specification {
       val actorNode = new ActorNode("A1", "Bruce Willis")
       val res = write(actorNode)
 
-      res.contains(""""normalMovie":null""") must beFalse
-      res.contains(""""lazyMovie":null""") must beFalse
-      res.contains(""""lazyMovies":null""") must beFalse
-      res.contains(""""rating":0""") must beFalse
-      res.contains(""""isBald":false""") must beFalse
+      assert(!res.contains(""""normalMovie":null"""))
+      assert(!res.contains(""""lazyMovie":null"""))
+      assert(!res.contains(""""lazyMovies":null"""))
+      assert(!res.contains(""""rating":0"""))
+      assert(!res.contains(""""isBald":false"""))
     }
 
     "Don't serialize lazy val to json if includeLazyVal is not set in FieldSerializer" in {
@@ -41,11 +41,11 @@ class LazyValBugs extends Specification {
       val actorNode = new ActorNode("A1", "Bruce Willis")
       val res = write(actorNode)
 
-      res.contains(""""normalMovie":null""") must beFalse
-      res.contains(""""lazyMovie":null""") must beTrue
-      res.contains(""""lazyMovies":null""") must beTrue
-      res.contains(""""rating":0""") must beTrue
-      res.contains(""""isBald":false""") must beTrue
+      assert(!res.contains(""""normalMovie":null"""))
+      assert(res.contains(""""lazyMovie":null"""))
+      assert(res.contains(""""lazyMovies":null"""))
+      assert(res.contains(""""rating":0"""))
+      assert(res.contains(""""isBald":false"""))
     }
 
     val jsonStr =
@@ -87,21 +87,21 @@ class LazyValBugs extends Specification {
 
       val actorNode = read[ActorNode](jsonStr)
 
-      actorNode.id === "A2"
-      actorNode.name === "Tom Hanks"
-      actorNode.normalMovie.id === "M13"
-      actorNode.normalMovie.title === "Apollo 13"
-      actorNode.lazyMovie.id === "M5"
-      actorNode.lazyMovie.title === "Forrest Gump"
-      actorNode.lazyMovies must have size 3
+      assert(actorNode.id == "A2")
+      assert(actorNode.name == "Tom Hanks")
+      assert(actorNode.normalMovie.id == "M13")
+      assert(actorNode.normalMovie.title == "Apollo 13")
+      assert(actorNode.lazyMovie.id == "M5")
+      assert(actorNode.lazyMovie.title == "Forrest Gump")
+      assert(actorNode.lazyMovies.size == 3)
       for (movie <- actorNode.lazyMovies) {
-        if (movie.id == "M6") movie.title === "Cast Away"
-        else if (movie.id == "M7") movie.title === "You've Got Mail"
-        else if (movie.id == "M8") movie.title === "The Da Vinci Code"
-        else failure("Invalid movie id found: " + movie.id)
+        if (movie.id == "M6") assert(movie.title == "Cast Away")
+        else if (movie.id == "M7") assert(movie.title == "You've Got Mail")
+        else if (movie.id == "M8") assert(movie.title == "The Da Vinci Code")
+        else fail("Invalid movie id found: " + movie.id)
       }
-      actorNode.rating === 5
-      actorNode.isBald === false
+      assert(actorNode.rating == 5)
+      assert(actorNode.isBald == false)
     }
 
     "Don't deserialize lazy val from parsed json if includeLazyVal is not set in FieldSerializer" in {
@@ -109,20 +109,20 @@ class LazyValBugs extends Specification {
 
       val actorNode = read[ActorNode](jsonStr)
 
-      actorNode.id === "A2"
-      actorNode.name === "Tom Hanks"
-      actorNode.normalMovie.id === "M13"
-      actorNode.normalMovie.title === "Apollo 13"
-      actorNode.lazyMovie.id === "M2"
-      actorNode.lazyMovie.title === "Armageddon"
-      actorNode.lazyMovies must have size 2
+      assert(actorNode.id == "A2")
+      assert(actorNode.name == "Tom Hanks")
+      assert(actorNode.normalMovie.id == "M13")
+      assert(actorNode.normalMovie.title == "Apollo 13")
+      assert(actorNode.lazyMovie.id == "M2")
+      assert(actorNode.lazyMovie.title == "Armageddon")
+      assert(actorNode.lazyMovies.size == 2)
       for (movie <- actorNode.lazyMovies) {
-        if (movie.id == "M3") movie.title === "Sixth Sense"
-        else if (movie.id == "M4") movie.title === "Unbreakable"
-        else failure("Invalid movie id found: " + movie.id)
+        if (movie.id == "M3") assert(movie.title == "Sixth Sense")
+        else if (movie.id == "M4") assert(movie.title == "Unbreakable")
+        else fail("Invalid movie id found: " + movie.id)
       }
-      actorNode.rating === 3
-      actorNode.isBald === true
+      assert(actorNode.rating == 3)
+      assert(actorNode.isBald == true)
     }
   }
 
