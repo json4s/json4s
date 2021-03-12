ce4bdf1dbd799e3fd721bd74a70cdf021e080fa2
kenji yoshida <6b656e6a69@gmail.com>
2021-03-12 23:14:34 +0900

    use scalatest

--- tests/src/test/scala/org/json4s/JSetExamples.scala
+++ tests/src/test/scala/org/json4s/JSetExamples.scala
@@ -3,32 +3,32 @@ package org.json4s
 /**
  * Created by pankhuri on 8/18/16.
  */
-import org.specs2.mutable._
+import org.scalatest.wordspec.AnyWordSpec
 
-class JSetExamples extends Specification {
+class JSetExamples extends AnyWordSpec {
 
   "Intersection of JSet" in {
     val set1 = JSet(Set(JString("g"), JBool(true)))
     val set2 = JSet(Set(JBool(true)))
-    set1 intersect set2 must_== set2
+    assert((set1 intersect set2) == set2)
   }
 
   "Union of JSet" in {
     val set1 = JSet(Set(JString("g"), JBool(true)))
     val set2 = JSet(Set(JBool(true)))
-    set1 union set2 must_== set1
+    assert((set1 union set2) == set1)
   }
 
   "Difference of JSet" in {
     val set1 = JSet(Set(JString("g"), JBool(true)))
     val set2 = JSet(Set(JBool(true)))
-    set1 difference set2 must_== JSet(Set(JString("g")))
+    assert((set1 difference set2) == JSet(Set(JString("g"))))
   }
 
   "Diff of JSet" in {
     val set1 = JSet(Set(JString("g")))
     val set2 = JSet(Set(JBool(true)))
-    set1 diff set2 must_== Diff(JNothing, set2, set1)
+    assert((set1 diff set2) == Diff(JNothing, set2, set1))
   }
 
 }
