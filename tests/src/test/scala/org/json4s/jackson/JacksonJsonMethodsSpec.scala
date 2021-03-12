7bf174ad44bdd67d18d0c226f4e1d3263d50dca0
kenji yoshida <6b656e6a69@gmail.com>
2021-03-12 23:11:07 +0900

    use scalatest

--- tests/src/test/scala/org/json4s/jackson/JacksonJsonMethodsSpec.scala
+++ tests/src/test/scala/org/json4s/jackson/JacksonJsonMethodsSpec.scala
@@ -1,45 +1,45 @@
 package org.json4s
 package jackson
 
-import org.specs2.mutable.Specification
+import org.scalatest.wordspec.AnyWordSpec
 
-class JacksonJsonMethodsSpec extends Specification {
+class JacksonJsonMethodsSpec extends AnyWordSpec {
 
   import org.json4s.JsonDSL._
   import JsonMethods._
 
   "JsonMethods.write" should {
-    "produce JSON without empty fields" in {
+    "produce JSON without empty fields" should {
 
       implicit val format = DefaultFormats.skippingEmptyValues
 
       "from Seq(Some(1), None, None, Some(2))" in {
         val seq = Seq(Some(1), None, None, Some(2))
         val expected = JArray(List(JInt(1), JNothing, JNothing, JInt(2)))
-        render(seq) must_== expected
+        assert(render(seq) == expected)
       }
 
       """from Map("a" -> Some(1), "b" -> None, "c" -> None, "d" -> Some(2))""" in {
         val map = Map("a" -> Some(1), "b" -> None, "c" -> None, "d" -> Some(2))
         val expected = JObject(List(("a", JInt(1)), ("b", JNothing), ("c", JNothing), ("d", JInt(2))))
-        render(map) must_== expected
+        assert(render(map) == expected)
       }
     }
 
-    "produce JSON with empty fields preserved" in {
+    "produce JSON with empty fields preserved" should {
 
       implicit val format = DefaultFormats.preservingEmptyValues
 
       "from Seq(Some(1), None, None, Some(2))" in {
         val seq = Seq(Some(1), None, None, Some(2))
         val expected = JArray(List(JInt(1), JNull, JNull, JInt(2)))
-        render(seq) must_== expected
+        assert(render(seq) == expected)
       }
 
       """from Map("a" -> Some(1), "b" -> None, "c" -> None, "d" -> Some(2))""" in {
         val map = Map("a" -> Some(1), "b" -> None, "c" -> None, "d" -> Some(2))
         val expected = JObject(List(("a", JInt(1)), ("b", JNull), ("c", JNull), ("d", JInt(2))))
-        render(map) must_== expected
+        assert(render(map) == expected)
       }
     }
 
