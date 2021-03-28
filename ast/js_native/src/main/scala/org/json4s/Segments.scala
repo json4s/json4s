3aec4091f8d86479a12c546004b42d95536da76c
xuwei-k <6b656e6a69@gmail.com>
2021-03-29 08:34:03 +0900

    add dummy methods in Segments

--- ast/js_native/src/main/scala/org/json4s/Segments.scala
+++ ast/js_native/src/main/scala/org/json4s/Segments.scala
@@ -1,11 +1,14 @@
 package org.json4s
 
 private[json4s] object Segments {
+  private[json4s] var segmentSize: Int = ParserUtil.defaultSegmentSize
   def apply(): Segment = {
-    Segment(new Array(ParserUtil.defaultSegmentSize))
+    Segment(new Array(segmentSize))
   }
 
   def release(s: Segment): Unit = ()
+
+  private[json4s] def clear(): Unit = ()
 }
 
 private[json4s] final case class Segment(val seg: Array[Char])
