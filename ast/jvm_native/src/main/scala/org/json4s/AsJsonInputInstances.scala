1518df20b258e88cf6efe6b113e9c64bd6c802d6
xuwei-k <6b656e6a69@gmail.com>
2021-05-26 09:30:49 +0900

    use SAM conversion

--- ast/jvm_native/src/main/scala/org/json4s/AsJsonInputInstances.scala
+++ ast/jvm_native/src/main/scala/org/json4s/AsJsonInputInstances.scala
@@ -3,6 +3,6 @@ package org.json4s
 trait AsJsonInputInstances { self: AsJsonInput.type =>
 
   implicit final val fileAsJsonInput: AsJsonInput[java.io.File] =
-    fromFunction(x => FileInput(x))
+    x => FileInput(x)
 
 }
--- ast/shared/src/main/scala/org/json4s/AsJsonInput.scala
+++ ast/shared/src/main/scala/org/json4s/AsJsonInput.scala
@@ -12,21 +12,20 @@ object AsJsonInput extends AsJsonInputInstances {
   def apply[A](implicit a: AsJsonInput[A]): AsJsonInput[A] = a
   def asJsonInput[A](input: A)(implicit a: AsJsonInput[A]): JsonInput = a.toJsonInput(input)
 
-  // TODO use SAM conversion if Scala 2.11 dropped
   def fromFunction[A](f: A => JsonInput): AsJsonInput[A] =
     new AsJsonInput[A] {
       def toJsonInput(a: A): JsonInput = f(a)
     }
 
   implicit val stringAsJsonInput: AsJsonInput[String] =
-    fromFunction(x => StringInput(x))
+    x => StringInput(x)
 
   implicit def readerAsJsonInput[A <: java.io.Reader]: AsJsonInput[A] =
-    fromFunction(x => ReaderInput(x))
+    x => ReaderInput(x)
 
   implicit def streamAsJsonInput[A <: java.io.InputStream]: AsJsonInput[A] =
-    fromFunction(x => StreamInput(x))
+    x => StreamInput(x)
 
   implicit val identity: AsJsonInput[JsonInput] =
-    fromFunction(x => x)
+    x => x
 }
