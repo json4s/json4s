99ca0d156e3751d2ae17d5b04fc082a29f60209a
kenji yoshida <6b656e6a69@gmail.com>
2023-06-03 09:17:08 +0900

    update scala versions

--- project/build.scala
+++ project/build.scala
@@ -53,8 +53,8 @@ object build {
     }
   )
 
-  val Scala212 = "2.12.17"
-  val Scala213 = "2.13.10"
+  val Scala212 = "2.12.18"
+  val Scala213 = "2.13.11"
   val Scala3 = "3.3.0"
 
   def json4sSettings(cross: Boolean) = mavenCentralFrouFrou ++ Def.settings(
