62dd16314b5e5e4dc7b4885c17f7159965add6d3
xuwei-k <6b656e6a69@gmail.com>
2023-09-09 09:07:15 +0900

    fix warning

--- native/src/test/scala/org/json4s/JsonFormatsSpec.scala
+++ native/src/test/scala/org/json4s/JsonFormatsSpec.scala
@@ -67,7 +67,7 @@ abstract class JsonFormatsSpec[T](mod: String) extends AnyWordSpec with TypeHint
     }
 
     "Unicode escaping can be changed" should {
-      val json = parse("""{"Script Small G": "\u210A"}""")
+      val json = parse("{\"Script Small G\": \"\u210A\"}")
 
       "escaped" in {
         assert(compact(render(json, alwaysEscapeUnicode = true)) == "{\"Script Small G\":\"\\u210A\"}")
