c4e5526ad00414f9ba49b334e8af0f53e4f00e41
xuwei-k <6b656e6a69@gmail.com>
2024-05-02 15:51:25 +0900

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,15 +16,15 @@ object Dependencies {
     "org.scalaz" %%% "scalaz-core" % "7.3.8"
   )
   lazy val paranamer = "com.thoughtworks.paranamer" % "paranamer" % "2.8"
-  private def scalatestVersion = "3.2.17"
+  private def scalatestVersion = "3.2.18"
   lazy val scalatest = Def.setting(
     Seq("org.scalatest" %%% "scalatest-wordspec" % scalatestVersion % "test")
   )
   lazy val scalatestScalacheck = Def.setting(
-    Seq("org.scalatestplus" %%% "scalacheck-1-17" % s"${scalatestVersion}.0" % "test")
+    Seq("org.scalatestplus" %%% "scalacheck-1-18" % s"${scalatestVersion}.0" % "test")
   )
 
   lazy val scalaXml = Def.setting {
-    "org.scala-lang.modules" %%% "scala-xml" % "2.1.0"
+    "org.scala-lang.modules" %%% "scala-xml" % "2.3.0"
   }
 }
