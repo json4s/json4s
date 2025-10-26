7b425a6ebcb442dbabd66d802a66a078d88eb82e
xuwei-k <6b656e6a69@gmail.com>
2025-10-26 10:19:08 +0900

    fix warnings

--- core/shared/src/main/scala/org/json4s/reflect/ParanamerReader.scala
+++ core/shared/src/main/scala/org/json4s/reflect/ParanamerReader.scala
@@ -1,10 +1,14 @@
 package org.json4s.reflect
 
+import scala.collection.immutable.ArraySeq
+
 object ParanamerReader extends ParameterNameReader {
   def lookupParameterNames(constructor: Executable): Seq[String] =
     constructor.getAsAccessibleObject match {
       case executable: java.lang.reflect.Executable =>
-        executable.getParameters.map(_.getName)
+        ArraySeq.unsafeWrapArray(
+          executable.getParameters.map(_.getName)
+        )
       case _ =>
         Nil
     }
