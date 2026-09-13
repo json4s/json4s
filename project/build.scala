707c446cc23d357479c59088dac0163b54ab1bb5
xuwei-k <6b656e6a69@gmail.com>
2026-09-13 16:47:40 +0900

    Fix warning

--- project/build.scala
+++ project/build.scala
@@ -126,6 +126,7 @@ object build {
           Seq("-Xsource:3-cross")
         case _ =>
           Seq(
+            "-Wconf:msg=is no longer supported for vararg splices:error",
             "-Wconf:msg=Implicit parameters should be provided with:error",
             "-Wconf:msg=with as a type operator has been deprecated:error"
           )
